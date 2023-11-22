2d7051bb4250722e319dda284e603065b2af174f
FlukeSupakorn <supakorn.tungpa@gmail.com>
2023-11-22 22:12:24 +0700

    Home(V1.1) Update
    Content

--- client/src/Component/Home/HomeContent.jsx
+++ client/src/Component/Home/HomeContent.jsx
@@ -4,8 +4,10 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Img = styled('img') ({
     borderRadius: 12,
@@ -32,7 +34,7 @@ const Item = styled(Paper)(({ theme }) => ({
 function HomeContent() {
   return (
     <Item>
-      <Grid item xs={2.5} style={{ paddingLeft: '10px' }}>
+      <Grid item xs={2.8} style={{ paddingLeft: '10px' }}>
         <Img
           src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
           loading="lazy"
@@ -42,10 +44,12 @@ function HomeContent() {
           }}
         />
       </Grid>
-      <Grid item xs={0.5} style={{ borderRight: '1px solid	#cdcdcd', height: '100%' }}/>
-      <Grid item container justifyContent="center" alignItems="center" xs={9}>
-        <Grid item container direction="column" xs={2} maxHeight="100px">
-            <Grid item xs={4}> text </Grid>
+      <Grid item xs={0.2} style={{ borderRight: '1px solid	#cdcdcd', height: '100%', marginRight: "20px"}}/>
+      <Grid item container justifyItems="center" alignItems="center" xs={9}>
+        <Grid item container xs={2} maxHeight="100px" direction="column" wrap='wrap' alignItems="flex-start">
+                <Grid item xs={4} fontSize={16} color='black' marginBottom="2px"> Central Rama 2 </Grid>
+                <Grid item xs={4} fontSize={10} marginBottom="3px"> 10 km from you </Grid>
+                <Grid item xs={4} fontSize={10} textAlign="left"> <LocationOnIcon style={{ fontSize: '10px', marginLeft: '1px', marginRight: '4px' }} /> Lorem ipsum dolor sit amet consectetur adipisicing elit. A, magni? asd asd adas ad asda asdadasa as da d </Grid>
         </Grid>
 
         <Grid item container xs={10} spacing={2} justifyContent="center" alignItems="center">
