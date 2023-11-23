18ce2feaf4f411edadcb02138443a1bfc8936d0a
Narutchai <koonfluk2015@gmail.com>
2023-11-23 22:33:00 +0700

    update

--- sirisoft-server/src/server.ts
+++ sirisoft-server/src/server.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+//import Zone
+import express from 'express';
+const cors = require('cors');
+
+
+
+
+// define zone
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// listen
+app.listen(port, () => {
+    console.log(`Server started on at http://localhost:${port}`);
+});
