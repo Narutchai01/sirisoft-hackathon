19e1ac5e477d86fa63c9ff79ccbfb353b15e09d3
Sarun khumthai <srnkhumthai@gmail.com>
2023-11-25 14:52:10 +0700

    add cal route

--- client/src/Component/Map/Map.jsx
+++ client/src/Component/Map/Map.jsx
@@ -1,52 +1,104 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import { Grid } from '@mui/material';
+import React from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { Grid } from "@mui/material";
 
 const containerStyle = {
-  width: '100%',
-  height: '100vh'
+  width: "100%",
+  height: "100vh",
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
+//Get user location
+function useUserLocation() {
+  const [location, setLocation] = React.useState({
+    lat: null,
+    lng: null,
+  });
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  return location;
+}
 
 const Map = () => {
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyCZBeJA2Iq-vVE3HmLe_xqw_g7S6YQIWmg"
-  })
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCZBeJA2Iq-vVE3HmLe_xqw_g7S6YQIWmg",
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
+  const [directionsResponse, setDirectionsResponse] = React.useState(null);
+  const [distance, setDistance] = React.useState("");
+  const [duration, setDuration] = React.useState("");
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
+    const bounds = new google.maps.LatLngBounds(location.lat, location.lng);
     map.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
 
   return isLoaded ? (
     <>
-      <Grid container direction="column" style={{ minHeight: '100%'}}>
+      <Grid container direction="column" style={{ minHeight: "100%" }}>
         <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-          >
-              <Marker position={center} />
-          </GoogleMap>
+          mapContainerStyle={containerStyle}
+          center={{ lat: location.lat, lng: location.lng }}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          <Marker position={{ lat: location.lat, lng: location.lng }} />
+          {/* {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )} */}
+        </GoogleMap>
       </Grid>
     </>
-  ) : <></>
-}
+  ) : (
+    <></>
+  );
+};
 
-export default Map
+export default Map;
