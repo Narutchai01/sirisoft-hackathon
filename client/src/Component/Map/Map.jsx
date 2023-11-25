a1561b5c7d065eaacff270cd20d5efee5394d328
tawayahc <yotachayawat@gmail.com>
2023-11-25 11:49:24 +0700

    Map update #1

--- client/src/Component/Map/Map.jsx
+++ client/src/Component/Map/Map.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { Grid } from '@mui/material';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 const Map = () => {
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: "AIzaSyCZBeJA2Iq-vVE3HmLe_xqw_g7S6YQIWmg"
-    });
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyCZBeJA2Iq-vVE3HmLe_xqw_g7S6YQIWmg"
+  })
 
-  return (
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
     <>
-        
+      <Grid container direction="column" style={{ minHeight: '100%'}}>
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+              <Marker position={center} />
+          </GoogleMap>
+      </Grid>
     </>
-  )
+  ) : <></>
 }
 
 export default Map
