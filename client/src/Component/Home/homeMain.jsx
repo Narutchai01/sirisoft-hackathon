ba47a9b5ab6d58b9e2d9aadd2108c348ba1784f8
Narutchai <koonfluk2015@gmail.com>
2023-11-25 16:44:57 +0700

    have bug in Plan
    มีบัคช่วยด้วย
    
    Co-Authored-By: FlukeSupakorn <125183790+FlukeSupakorn@users.noreply.github.com>

--- client/src/Component/Home/homeMain.jsx
+++ client/src/Component/Home/homeMain.jsx
@@ -1,21 +1,50 @@
 // import * as React from "react";
 import Grid from "@mui/material/Grid";
-// import HomeContent from "./HomeContent";
-// import MallData from "../Data/MallData.js";
+import HomeContent from "./HomeContent";
+import MallData from "../Data/MallData.js";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function HomeMain() {
-    const getMallData = mallDataObj => {
-        return (
-            <Grid item xs={4}>
-                <HomeContent {...mallDataObj}/>
-            </Grid>
-        );
-    }
+
+  const [location, setLocation] = useState({
+    lat: 0,
+    lng: 0
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+
+  useEffect(() => {
+    const sendLocation = async () => {
+      if (location.lat === 0 && location.lng === 0) {
+        return false;
+      }
+      await axios.post("http://localhost:3000/api/calculatedistance", location).then((res) => {
+        console.log(res.data);
+        console.log(location.lat, location.lng);
+      });
+    };
+    sendLocation();
+  }, [location]);
+
+  const getMallData = mallDataObj => {
     return (
-        <Grid container direction="column" spacing={3}>  
-            {MallData.map(mallDataObjl => getMallData(mallDataObjl))}
-        </Grid>
+      <Grid item xs={4}>
+        <HomeContent {...mallDataObj} />
+      </Grid>
     );
+  }
+  return (
+    <Grid container direction="column" spacing={3}>
+      {MallData.map(mallDataObjl => getMallData(mallDataObjl))}
+    </Grid>
+  );
 }
