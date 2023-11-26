f4d1f128f55aeab1ea091a3aa8bc85f558ae6bdf
FlukeSupakorn <supakorn.tungpa@gmail.com>
2023-11-26 09:52:58 +0700

    Store Search

--- client/src/Component/Mall/MallMain.jsx
+++ client/src/Component/Mall/MallMain.jsx
@@ -1,57 +1,67 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
-export default function MallMain() {
-    const [selectedFloor, setSelectedFloor] = React.useState('1');
+const MallMain = ({ shopFloor, onFloorChange }) => {
+  const floorOptions = [
+    { floor: '1', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_1-scaled.jpg' },
+    { floor: '2', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_2-scaled.jpg' },
+    { floor: '3', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_3-scaled.jpg' },
+    { floor: '4', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_4-scaled.jpg' },
+    { floor: '5', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_5-scaled.jpg' },
+    { floor: '6', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_6-scaled.jpg' },
+    { floor: '7', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_7-scaled.jpg' },
+  ];
 
-    const handleFloorChange = (event) => {
-        setSelectedFloor(event.target.value);
-    };
+  const [selectedFloor, setSelectedFloor] = useState('1');
 
-    const floorOptions = [
-        { floor: '1', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_1-scaled.jpg' },
-        { floor: '2', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_2-scaled.jpg' },
-        { floor: '3', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_3-scaled.jpg' },
-        { floor: '4', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_4-scaled.jpg' },
-        { floor: '5', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_5-scaled.jpg' },
-        { floor: '6', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_6-scaled.jpg' },
-        { floor: '7', image: 'http://mallbangkok.com/wp-content/uploads/2023/04/centralworld_7-scaled.jpg' },
-    ];
+  // Set the initial floor based on the shopFloor prop
+  useEffect(() => {
+    setSelectedFloor(shopFloor || '1');
+  }, [shopFloor]);
 
-return (
+  const handleFloorChange = (event) => {
+    const newFloor = event.target.value;
+    setSelectedFloor(newFloor);
+    // Notify the parent component about the floor change
+    onFloorChange && onFloorChange(newFloor);
+  };
+
+  return (
     <Grid container spacing={2}>
-    <Grid item xs={6}>
+      <Grid item xs={6}>
         <TextField
-            fullWidth
-            select
-            label="Select Floor"
-            variant="outlined"
-            value={selectedFloor}
-            onChange={handleFloorChange}
+          fullWidth
+          select
+          label="Select Floor"
+          variant="outlined"
+          value={selectedFloor}
+          onChange={handleFloorChange}
         >
-        {floorOptions.map((option) => (
+          {floorOptions.map((option) => (
             <MenuItem key={option.floor} value={option.floor}>
-                {option.floor}
+              {option.floor}
             </MenuItem>
-        ))}
+          ))}
         </TextField>
-    </Grid>
-        <div
+      </Grid>
+      <div
         style={{
-            width: '100%',
-            padding: '20px',
-            marginBottom: '20px',
-            overflowX: 'auto',
+          width: '100%',
+          padding: '20px',
+          marginBottom: '20px',
+          overflowX: 'auto',
         }}
-        >
+      >
         <img
-            style={{ width: '100%', height: 'auto' }}
-            src={floorOptions.find((option) => option.floor === selectedFloor)?.image || ''}
-            alt={`Floor ${selectedFloor}`}
+          style={{ width: '100%', height: 'auto' }}
+          src={floorOptions.find((option) => option.floor === selectedFloor)?.image || ''}
+          alt={`Floor ${selectedFloor}`}
         />
-        </div>
+      </div>
     </Grid>
-    );
-}
+  );
+};
+
+export default MallMain;
--- client/src/Component/Search/StoreSearch.jsx
+++ client/src/Component/Search/StoreSearch.jsx
@@ -1,74 +1,103 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import { Input as BaseInput } from '@mui/base/Input';
+import MallData from '../Data/MallData';
+import Typography from '@mui/material/Typography';
 
 const Input = React.forwardRef(function CustomInput(props, ref) {
-    return <BaseInput slots={{ input: InputElement }} {...props} ref={ref} />;
+  return <BaseInput slots={{ input: InputElement }} {...props} ref={ref} />;
 });
 
 const blue = {
-    100: '#DAECFF',
-    200: '#b6daff',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    900: '#003A75',
+  100: '#DAECFF',
+  200: '#b6daff',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  900: '#003A75',
 };
 
 const grey = {
-    50: '#F3F6F9',
-    100: '#E5EAF2',
-    200: '#DAE2ED',
-    300: '#C7D0DD',
-    400: '#B0B8C4',
-    500: '#9DA8B7',
-    600: '#6B7A90',
-    700: '#434D5B',
-    800: '#303740',
-    900: '#1C2025',
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
 };
 
-const InputElement = styled('input')(
-    ({ theme }) => `
+const InputElement = styled('input')(({ theme }) => `
     font-family: IBM Plex Sans, sans-serif;
     font-size: 0.875rem;
     font-weight: 400;
     line-height: 1.5;
     padding: 16px;
-    width: 92%;
+    width: 16.75vw;
     border-radius: 8px;
     color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
     background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
     border: 1px solid #C4C4C4;
-    box-shadow: 0px 2px 4px ${
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.5)' : 'rgba(0,0,0, 0.05)'
-    };
+    box-shadow: 0px 2px 4px ${theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.5)' : 'rgba(0,0,0, 0.05)'};
 
     &:hover {
-    border-color: ${blue[400]};
+        border-color: ${blue[400]};
     }
 
     &:focus {
-    border-color: ${blue[400]};
-    transition: all 0.15s ease-in-out;
-    scale: 1.1;
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+        border-color: ${blue[400]};
+        transition: all 0.15s ease-in-out;
+        scale: 1.1;
+        box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
     }
 
     // firefox
     &:focus-visible {
-    outline: 0;
+        outline: 0;
     }
-`,
-);
+`);
 
-    const StoreSearch = () => {
-    return (
-        <Grid>
-            <Input placeholder="Searh Store..." />
+const StoreSearch = ({ mallName, onShopClick }) => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleClickShop = (shop) => {
+    onShopClick(shop.shopFloor);
+  };
+
+  const filteredStores = MallData
+    .filter(mall => mall.mallName === mallName)
+    .flatMap(mall => mall.storeData)
+    .filter(store => store.shopName.toLowerCase().includes(searchInput.toLowerCase())); // Filter by search input
+
+  return (
+    <Grid container>
+      <Input
+        placeholder="Search Store..."
+        onChange={handleSearchChange}
+      />
+      {searchInput && (
+        <Grid container borderRadius={1} boxShadow={2} style={{ borderRadius: '' }} xs={12}>
+          {filteredStores.map((store, index) => (
+            <Grid item container style={{ display: 'flex' }} padding={2} key={index} onClick={() => handleClickShop(store)}>
+                <Grid item container paddingLeft={5} xs={8}>
+                    <Typography item xs={12} color='grey'>{store.shopName}</Typography>
+                </Grid>
+                <Grid item xs={4} color='grey' textAlign='center'> Floor: {store.shopFloor} </Grid>
+                <Grid item xs={12} padding={0.5} borderBottom='1px solid #AAA'></Grid>
+            </Grid>
+          ))}
         </Grid>
-    )
-}
+      )}
+    </Grid>
+  );
+};
 
-export default StoreSearch
+export default StoreSearch;
